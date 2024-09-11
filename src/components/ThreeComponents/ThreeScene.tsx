eb53ae78710ac2a9b3a573e95b2593988e6f3b2e
Yurii Trach <yurii.trach@ubisoft.com>
2024-09-12 02:34:27 +0300

    pre change

--- src/components/ThreeComponents/ThreeScene.tsx
+++ src/components/ThreeComponents/ThreeScene.tsx
@@ -5,8 +5,8 @@ import Model from "@/components/ThreeComponents/Model";
 import {Suspense, useRef} from "react";
 import {Center, Environment, Html, OrbitControls, useProgress} from "@react-three/drei";
 import styles from "@/styles/Background.module.css";
-import {EffectComposer, Noise, Outline, SSAO, N8AO, SMAA} from "@react-three/postprocessing";
-import {BlendFunction} from "postprocessing";
+import {EffectComposer, Noise, Outline, SSAO, N8AO, SMAA, ColorAverage} from "@react-three/postprocessing";
+// import {BlendFunction} from "postprocessing";
 
 function Loader() {
     const {progress} = useProgress()
@@ -19,19 +19,20 @@ const ThreeScene = () => {
 
     return (
         <Canvas gl={{antialias: true}} dpr={[1, 1.5]} className={styles.canvas}>
-            {/*<ambientLight/>*/}
+            {/*<ambientLight intensity={1}/>*/}
             <Suspense fallback={<Loader/>}>
                 <Center>
-                    {/*<mesh>*/}
-                    {/*    <boxGeometry />*/}
-                    {/*    <meshStandardMaterial />*/}
-                    {/*</mesh>*/}
                     <Model/>
                 </Center>
             </Suspense>
-            <Environment preset={"studio"} background={false} environmentIntensity={0.5} />
+            <Environment files={"/textures/studio_small.exr"} background={false} environmentIntensity={1} resolution={16} />
             <EffectComposer enableNormalPass={true}>
-                {/*<SSAO/>*/}
+                {/*<ColorAverage/>*/}
+                <SSAO
+                    samples={30}
+                    rings={10}
+                    distanceThreshold={1.0}
+                />
                 {/*<N8AO halfRes color="black" aoRadius={2} intensity={1} aoSamples={6} denoiseSamples={4} />*/}
             </EffectComposer>
 
