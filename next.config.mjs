e330eb11087863d742c6d8ee550f9666f8ef1d32
Yurii Trach <yurii.trach@ubisoft.com>
2024-09-14 19:43:51 +0300

    test global deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     transpilePackages: ['three'],
-    output: "export",
-    basePath: "/AAA_TEST",
+    // output: "export",
+    // basePath: "/AAA_TEST",
 };
 
 export default nextConfig;
